65b910372a2ff662edaa17e815a6806ec4cf176c
Jeffrey Liu <17771770679@163.com>
2019-08-13 23:19:18 +0800

    简单介绍下多入口配置以及output中publicPath的用法，，详细内容可看webpack官网中的configuration

--- webpack.config.js
+++ webpack.config.js
@@ -48,6 +48,8 @@ const config = {
     },
     output:{
         filename:'[name].js',
+        // 后台用index.html,静态资源放到CDN，此处可配置CDN地址，会在打包好的文件前边加上下方地址
+        publicPath: 'https://www.baidu.com',
         path:path.resolve(__dirname,'dist')
     },
     plugins: [
