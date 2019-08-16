bb2ecd86cd6e2229c73e2b660077c98ca4385137
Jeffrey Liu <17771770679@163.com>
2019-08-16 22:46:36 +0800

    清除之前文件

--- src/content.js
+++
@@ -1,6 +0,0 @@
-export default function Content () {
-    var dom = document.getElementById('root')
-    var content = document.createElement('div')
-    content.innerText = 'Content'
-    dom.append(content)
-}
--- src/sidebar.js
+++
@@ -1,6 +0,0 @@
-export default function SideBar () {
-    var content = document.createElement('div')
-    content.setAttribute('id','number')
-    content.innerText = 'SideBar'
-    document.body.append(content)
-}
